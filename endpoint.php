56f5d5b6e71cca550da45ef2b921b6c93f8e6024
tokidokinihongo <164835715+tokidokinihongo@users.noreply.github.com>
2024-06-24 13:51:10 +1000

    Update prepared statement

--- endpoint.php
+++ endpoint.php
@@ -2,11 +2,14 @@
 
 $url = $_SERVER['REQUEST_URI'];
 $trimmed_url = explode('/', $url);
+$trimmed_url = "%".$trimmed_url[3]."%";
 
 try {
     require ('dbconn.php');
-    $query = "SELECT link_input FROM links WHERE link_output LIKE '%$trimmed_url[3]';";
-    $result = $dbConn->query($query);
+    $query = $dbConn->prepare("SELECT link_input FROM links WHERE link_output LIKE ?");
+    $query->bind_param("s", $trimmed_url);
+    $query->execute();
+    $result = $query->get_result();
     if ($result->num_rows >= 1) {
         while ($row = $result->fetch_assoc()) {
             $link = $row['link_input'];
